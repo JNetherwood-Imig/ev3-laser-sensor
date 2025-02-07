ca41aeb7440dbbf905f7f6498bc3a1881d5bdd1a
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-02-07 13:09:41 -0800

    100ms read throttling

--- laser_sensor.py
+++ laser_sensor.py
@@ -1,9 +1,15 @@
 from pybricks.iodevices import I2CDevice
+import time
 
 class LaserSensor:
     def __init__(self, port):
         self.i2c = I2CDevice(port, 0x02 >> 1)
+        self.last_time = 0
+        self.last_dist = 0
 
     def distance(self):
-        results = self.i2c.read(0x42, 2)
-        return results[0] + (results[1] << 8)
+        if time.monotonic() - self.last_time > 0.1:
+            self.last_time = time.monotonic()
+            results = self.i2c.read(0x42, 2)
+            self.last_dist = results[0] + (results[1] << 8)
+        return self.last_dist
