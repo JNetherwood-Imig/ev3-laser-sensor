eb66df18ffd5752f0a37a6978d6b8290ff237c63
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-02-07 12:44:32 -0800

    updated distance() method name to match ultrasonic sensor

--- laser_sensor.py
+++ laser_sensor.py
@@ -4,6 +4,6 @@ class LaserSensor:
     def __init__(self, port):
         self.i2c = I2CDevice(port, 0x02 >> 1)
 
-    def get_distance(self):
+    def distance(self):
         results = self.i2c.read(0x42, 2)
         return results[0] + (results[1] << 8)
